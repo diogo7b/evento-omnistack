f808149a290d5b5b8c886880e59c729070ca7c9e
Diogo <diogosb74@gmail.com>
2020-03-27 21:15:14 -0300

    Adcionado backend funcional e frontend incompleto

---
+++ backend/src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = express.Router();
+
+
+const ongController = require('./controllers/ongController');
+const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
+
+
+
+routes.post('/sessions', sessionController.create);
+
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+
+routes.post('/incidents', incidentController.create);
+routes.get('/incidents', incidentController.index);
+routes.delete('/incidents/:id', incidentController.delete);
+
+routes.get('/profile/:id', profileController.index);
+
+
+
+module.exports = routes;
